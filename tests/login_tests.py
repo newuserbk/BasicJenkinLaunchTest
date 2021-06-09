d874beebcd47255630690ab42465ace5f7643f99
bharat.kodalkar <bharat.kodalkar@avalara.com>
2021-06-09 19:42:59 +0530

    added code

---
+++ tests/login_tests.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+import chromedriver_binary  # Adds chromedriver binary to path
+
+
+class Test1_login:
+
+    def test_setup(self):
+        print("Test-1")
+        print("Initializing driver")
+        global driver
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(5)
+        print("Maximizing browser window")
+        driver.maximize_window()
+
+    def test_login(self):
+        print("Test-2")
+        print("Launching driver")
+        driver.get("https://www.google.com")
+        time.sleep(4)
+        assert "Google" in driver.title
+
+    def test_tear_down(self):
+        print("Test-3")
+        print("closing browser")
+        driver.close()
+        driver.quit()
+
+
+
